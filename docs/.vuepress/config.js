fae6244d0ac1b3fb10520f5689034fa35d666a63
Kwok Hing <2801379112@qq.com>
2023-04-11 01:16:07 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "小G的Blog",
+  lastUpdated: "更新时间", // string | boolean
+  themeConfig: { 
+    description: " My First Blog",
+    logo: "/assets/img/hero.png",
+    head: [
+      ["meta", { author: "Kwok", content: "小G的个人博客" }],
+      ["head", { link: "./public/assets/img/favicon.ico" }],
+    ],
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "日记", link: "/diary/" },
+      { text: "技术", link: "/technology/" },
+      { text: "Github", link: "https://github.com/Chowhound-G" },
+    ],
+    sidebar: "auto",
+    displayAllHeaders: true,
+  },
+};
---
+++ docs/diary.md
@@ -0,0 +1,48 @@
+<!-- ---
+sidebar:auto;
+--- -->
+# 日记
+
+
+## 学习
+dfdasfd
+#### dfadsf
+## 生活
+dasfsdaf
+## 爱好
+
+#### 123
+asdf
+
+## 学习
+dfdasfd
+#### dfadsf
+## 生活
+dasfsdaf
+## 爱好
+
+#### 123
+## 学习
+dfdasfd
+#### dfadsf
+## 生活
+dasfsdaf
+## 爱好
+
+#### 123
+## 学习
+dfdasfd
+#### dfadsf
+## 生活
+dasfsdaf
+## 爱好
+
+#### 123
+## 学习
+dfdasfd
+#### dfadsf
+## 生活
+dasfsdaf
+## 爱好
+
+#### 123
